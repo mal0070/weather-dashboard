d533841d412b3c70223311ec114e4f6c55c3fec2
Mina Lee <mal0070@naver.com>
2024-11-12 17:36:44 +0900

    feat: setup project

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className='page'>
+        <div className='page__container'>
+            <header></header>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
